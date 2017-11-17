1a9f6f6172d609eef72e80bb4ff4845988367b85
sp9103 <sp9103@naver.com>
2017-11-17 19:03:13 +0900

    LunarLander code commit
    It is not complete version....
    It will be added Dual DQN

---
+++ LunarLander/DDQN/Main.py
@@ -0,0 +1,144 @@
+import tensorflow as tf
+import numpy as np
+import random
+import time
+import gym
+
+from Model import DQN
+
+tf.app.flags.DEFINE_string("train", "",
+                           """method to train, """
+                           """1. DQN, """
+                           """2. Double DQN, """
+                           """ex) --train=DQN.""")
+FLAGS = tf.app.flags.FLAGS
+
+MAX_EPISODE = 40000
+OBSERVE = 100
+TRAIN_INTERVAL = 4
+TARGET_UPDATE_INTERVAL = 1000
+
+def train():
+    print('let\'s start train')
+
+    sess = tf.Session()
+
+    game = gym.make('LunarLander-v2')
+    NUM_ACTION = 4
+    DIM_STATE = game.observation_space.shape[0]
+
+    net = DQN(sess, DIM_STATE, NUM_ACTION)  # example has 4 state
+
+    rewards = tf.placeholder(tf.float32, [None])
+    tf.summary.scalar('avg.reward/ep.', tf.reduce_mean(rewards))
+
+    saver = tf.train.Saver(tf.global_variables())
+    ckpt = tf.train.get_checkpoint_state('./model')
+    if ckpt and tf.train.checkpoint_exists(ckpt.model_checkpoint_path):
+        saver.restore(sess, ckpt.model_checkpoint_path)
+    else:
+        sess.run(tf.global_variables_initializer())
+
+    writer = tf.summary.FileWriter('logs', sess.graph)
+    summary_merged = tf.summary.merge_all()
+
+    epsilon = 1.0
+    time_step = 0
+    cost = 0.0
+    total_reward_list = []
+
+    for episode in range(MAX_EPISODE):
+        terminal = False
+        total_reward = 0
+
+        state = game.reset()
+        net.init_state(state)
+
+        while not terminal:
+            if np.random.rand() < epsilon:
+                action = random.randrange(NUM_ACTION)
+            else:
+                action = net.get_action()
+
+            state, reward, terminal, info = game.step(action)
+            total_reward += reward
+
+            net.remember(state, action, reward, terminal)
+
+            if time_step > OBSERVE and time_step % TRAIN_INTERVAL == 0:
+               if FLAGS.train == "DQN":
+                   cost = net.train()
+               elif FLAGS.train == "DDQN":
+                   cost = net.train()
+                ##
+
+            if time_step % TARGET_UPDATE_INTERVAL == 0:
+                net.update_target_network()
+
+            time_step += 1
+
+        print('게임횟수: %d 점수: %.1f cost : %f' % (episode + 1, total_reward, cost))
+
+        total_reward_list.append(total_reward)
+
+        if episode % 10 == 0:
+            summary = sess.run(summary_merged, feed_dict={rewards: total_reward_list})
+            writer.add_summary(summary, time_step)
+            total_reward_list = []
+
+        if episode % 100 == 0:
+            saver.save(sess, 'model/dqn.ckpt', global_step=time_step)
+
+    game.close()
+
+
+def replay():
+    print('let\'s start train')
+    #sess = tf.Session()
+
+    game = gym.make('LunarLander-v2')
+    #NUM_ACTION = game.action_space.size.count()   <-action space의 사이즈 어떻게 받는지 알면 알려주셈
+    NUM_ACTION = 4
+    NUM_STATE = game.observation_space.shape[0]
+    #net = DQN(sess, NUM_STATE, NUM_ACTION)  # example has 4 state
+
+    #saver = tf.train.Saver()
+    #ckpt = tf.train.get_checkpoint_state('model')
+    #saver.restore(sess, ckpt.model_checkpoint_path)
+
+    for episode in range(MAX_EPISODE):
+        terminal = False
+
+        state = game.reset()
+        #net.init_state(state)
+
+        while not terminal:
+            #action = net.get_action()
+            action = random.randrange(NUM_ACTION)
+            state, reward, terminal, info = game.step(action)
+            #net.remember(state, action, reward, terminal)
+
+            time.sleep(0.1)
+            game.render()
+
+    game.close()
+
+def main(_):
+    if FLAGS.train:
+        if FLAGS.train == "DQN":
+            print("DQN train start")
+            train()
+        elif FLAGS.train == "DDQN":
+            print("Double DQN train start")
+            train()
+        elif FLAGS.train == "DuelDQN":
+            print("Duel DQN train start")
+            train()
+        else:
+            print("Invalid option\n"
+                      "DQN or Double DQN or Duel DQN")
+    else:
+        replay()
+
+if __name__ == '__main__':
+    tf.app.run()
---
+++ LunarLander/DDQN/Model.py
@@ -0,0 +1,139 @@
+import tensorflow as tf
+import numpy as np
+import random
+from collections import deque
+
+class DQN:
+    REPLAY_MEMORY = 10000
+    BATCH_SIZE = 64
+    GAMMA = 0.99
+
+    def __init__(selfs, session, dim, n_action):
+        selfs.session = session
+        selfs.n_action = n_action
+        selfs.memory = deque()
+        selfs.state = None
+        selfs.dimension = dim           #state dimesion
+
+        selfs.input_X = tf.placeholder(tf.float32, [None, dim])
+        selfs.input_A = tf.placeholder(tf.int64, [None])
+        selfs.input_Y = tf.placeholder(tf.float32, [None])
+
+        selfs.Q = selfs._build_network('main')
+        selfs.cost, selfs.train_op = selfs._build_op()
+
+        selfs.target_Q = selfs._build_network('target')
+
+    def _build_network(selfs, name):
+        with tf.variable_scope(name):
+            model = tf.layers.dense(selfs.input_X, 32, activation=tf.nn.relu)
+            model = tf.layers.dense(model, 32, activation=tf.nn.relu)
+            model = tf.layers.dense(model, 16, activation=tf.nn.relu)
+
+            Q = tf.layers.dense(model, selfs.n_action, activation=None)
+
+        return Q
+
+    def _build_op(self):
+        # it will be updated DQN -> Doubling DQN
+        one_hot = tf.one_hot(self.input_A, self.n_action, 1.0, 0.0)
+        Q_value = tf.reduce_sum(tf.multiply(self.Q, one_hot), axis=1)
+        cost = tf.reduce_mean(tf.square(self.input_Y - Q_value))
+        train_op = tf.train.AdamOptimizer(1e-4).minimize(cost)
+
+        return cost, train_op
+
+    def update_target_network(self):
+        copy_op = []
+
+        main_vars = tf.get_collection(tf.GraphKeys.TRAINABLE_VARIABLES, scope='main')
+        target_vars = tf.get_collection(tf.GraphKeys.TRAINABLE_VARIABLES, scope='target')
+
+        for main_var, target_var in zip(main_vars, target_vars):
+            copy_op.append(target_var.assign(main_var.value()))
+
+        self.session.run(copy_op)
+
+    def get_action(self):
+        Q_value = self.session.run(self.Q,
+                                   feed_dict={self.input_X: [self.state]})
+
+        action = np.argmax(Q_value[0])
+
+        return action
+
+    def init_state(self, state):
+        self.state = state
+
+    def remember(self, state, action, reward, terminal):
+        next_state = state
+
+        self.memory.append((self.state, next_state, action, reward, terminal))
+
+        if len(self.memory) > self.REPLAY_MEMORY:
+            self.memory.popleft()
+
+        self.state = next_state
+
+    def _sample_memory(self):
+        sample_memory = random.sample(self.memory, self.BATCH_SIZE)
+
+        state = [memory[0] for memory in sample_memory]
+        next_state = [memory[1] for memory in sample_memory]
+        action = [memory[2] for memory in sample_memory]
+        reward = [memory[3] for memory in sample_memory]
+        terminal = [memory[4] for memory in sample_memory]
+
+        return state, next_state, action, reward, terminal
+
+    def train(self):
+        state, next_state, action, reward, terminal = self._sample_memory()
+
+        target_Q_value = self.session.run(self.target_Q,
+                                          feed_dict={self.input_X: next_state})
+
+        # DQN 의 손실 함수에 사용할 핵심적인 값을 계산하는 부분입니다. 다음 수식을 참고하세요.
+        # if episode is terminates at step j+1 then r_j
+        # otherwise r_j + γ*max_a'Q(ð_(j+1),a';θ')
+        # input_Y 에 들어갈 값들을 계산해서 넣습니다.
+        Y = []
+        for i in range(self.BATCH_SIZE):
+            if terminal[i]:
+                Y.append(reward[i])
+            else:
+                Y.append(reward[i] + self.GAMMA * np.max(target_Q_value[i]))
+
+        _, cost_val = self.session.run([self.train_op, self.cost],
+                         feed_dict={
+                             self.input_X: state,
+                             self.input_A: action,
+                             self.input_Y: Y
+                         })
+
+        return cost_val
+
+    def train_DDQN(self):
+        state, next_state, action, reward, terminal = self._sample_memory()
+
+        target_Q_value = self.session.run(self.target_Q,
+                                          feed_dict={self.input_X: next_state})
+        Q_value = self.session.run(self.Q,
+                                          feed_dict={self.input_X: next_state})
+        main_action = np.argmax(Q_value, axis=1)
+
+        Y = []
+        for i in range(self.BATCH_SIZE):
+            if terminal[i]:
+                Y.append(reward[i])
+            else:
+                Q_prime = target_Q_value[i][main_action[i]]
+                Y.append(reward[i] + self.GAMMA * Q_prime)
+
+        _, cost_val = self.session.run([self.train_op, self.cost],
+                                       feed_dict={
+                                           self.input_X: state,
+                                           self.input_A: action,
+                                           self.input_Y: Y
+                                       })
+
+        return cost_val
