35de4a89dee9a4fa6e87b50869b88126ea746748
sp9103 <sp9103@naver.com>
2017-12-07 18:14:08 +0900

    Batch update bug fix
    Batch Update가 아직 memory가 차기전에 수행되서는 안된다.

--- CustomExample/SumTree.py
+++ CustomExample/SumTree.py
@@ -125,8 +125,9 @@ class Memory(object):
         return b_idx, b_memory, ISWeights
 
     def batch_update(self, tree_idx, abs_errors):
-        abs_errors += self.epsilon  # convert to abs and avoid 0
-        clipped_errors = np.minimum(abs_errors, self.abs_err_upper)
-        ps = np.power(clipped_errors, self.alpha)
-        for ti, p in zip(tree_idx, ps):
-            self.tree.update(ti, p)
+        if self.tree.IsFull:
+            abs_errors += self.epsilon  # convert to abs and avoid 0
+            clipped_errors = np.minimum(abs_errors, self.abs_err_upper)
+            ps = np.power(clipped_errors, self.alpha)
+            for ti, p in zip(tree_idx, ps):
+                self.tree.update(ti, p)
