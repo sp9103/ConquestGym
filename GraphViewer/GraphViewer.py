36090bbf216a874a27b80a9f8be95a4ef8b6b968
sp9103 <sp9103@naver.com>
2017-12-13 19:52:42 +0900

    Moving average filter add

--- GraphViewer/GraphViewer.py
+++ GraphViewer/GraphViewer.py
@@ -2,6 +2,21 @@ import matplotlib.pyplot as plt
 import csv
 import argparse
 import os
+import numpy as np
+
+SMOOTHING_INTERVAL = 5
+
+def smoothing(src):
+    aver_box = np.zeros((SMOOTHING_INTERVAL, ), dtype=np.float64)
+
+    for i in range(0, len(src)):
+        aver_box = np.append(aver_box[1:], float(src[i]))
+
+        if i < SMOOTHING_INTERVAL - 1:
+            continue
+
+        mean_val = np.mean(aver_box)
+        src[i] = mean_val
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
@@ -11,7 +26,7 @@ if __name__ == '__main__':
     path  = args.path
 
     pieces = []
-    style = ['r^--', 'bs-']
+    style = [ 'bs-', 'r^--']
     style_idx = 0
     filenames = os.listdir(path)
     for filename in filenames:
@@ -29,6 +44,7 @@ if __name__ == '__main__':
         Value = [Value for WallTime, Step, Value in pieces]
 
         # 만약 그래프 스무딩이 필요하면 여기서 해야함 -TO-DO
+        smoothing(Value)
 
         plt.plot(episode, Value, style[style_idx], label=filename)
         style_idx += 1
